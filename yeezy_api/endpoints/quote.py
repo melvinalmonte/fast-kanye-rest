49458f96613cf2c98bbbe74b9c286cb9214b0c45
Melvin A <melvinalmonte@gmail.com>
2022-06-10 23:56:54 -0400

    added vercel deployment config

--- yeezy_api/endpoints/quote.py
+++ yeezy_api/endpoints/quote.py
@@ -7,16 +7,16 @@ router = APIRouter()
 
 
 class Quote(BaseModel):
-    ye_wisdom: str
+    ye_quote: str
 
 
-@router.get(path='/ye_says', status_code=status.HTTP_200_OK, response_model=Quote)
+@router.get(path='/ye', status_code=status.HTTP_200_OK, response_model=Quote)
 def generate_quote(background_tasks: BackgroundTasks):
     background_tasks.add_task(tweet_list)
     tweet = random_tweet()
-    return dict(ye_wisdom=tweet)
+    return dict(ye_quote=tweet)
 
 
 @router.get(path='/', status_code=status.HTTP_200_OK)
 def root():
-    return dict(message='Welcome to fast-kanye-rest! Use /ye_says to get some Jeen-Yuhs quotes.')
+    return dict(message='Welcome to fast-kanye-rest! Use /ye to get some Jeen-Yuhs quotes. Or use /api/docs for swagger docs.')
